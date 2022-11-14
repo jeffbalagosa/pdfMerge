e715f52cfb36e757bfa0ac8fdc49f17529ad7f88
JeffBalagosa <jeff.balagosa@corp.vegas.com>
2022-11-14 10:37:35 -0800

    remove .pdf as a choice for naming.  Add another conditional for .PDFs

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import os
 input_folder = 'input'
 # prompt user for output file name use _merged.pdf as default
 output_file_name = input(
-    'Enter output file name (_merged.pdf by default): ') or '_merged.pdf'
+    'Enter output file name (_merged.pdf by default): ') or '_merged'
 
 # merge all pdf files in the /input folder
 merger = PdfMerger()
 for filename in os.listdir(input_folder):
-    if filename.endswith('.pdf'):
+    if filename.endswith('.pdf') or filename.endswith('.PDF'):
         merger.append(f'{input_folder}/' + filename)
 
-merger.write(f'output/{output_file_name}')
+merger.write(f'output/{output_file_name}.pdf')
 merger.close()
 
 # ask if user wants to delete the files in the /input folder
 delete_files = input('Permanently delete files in input folder? (y/n): ')
 if delete_files.lower() == 'y':
     for filename in os.listdir(input_folder):
-        if filename.endswith('.pdf'):
+        if filename.endswith('.pdf' or '.PDF') or filename.endswith('.PDF'):
             os.remove(f'{input_folder}/' + filename)
