bffaea891051e3d0b6446857331c6c0849904955
JeffBalagosa <jeff.balagosa@corp.vegas.com>
2022-11-14 09:53:29 -0800

    Add options for users.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from PyPDF2 import PdfMerger
 import os
 
 input_folder = 'input'
-output_file_name = '0_merged_output.pdf'
+# prompt user for output file name use _merged.pdf as default
+output_file_name = input(
+    'Enter output file name (_merged.pdf by default): ') or '_merged.pdf'
 
 # merge all pdf files in the /input folder
 merger = PdfMerger()
@@ -12,3 +14,10 @@ for filename in os.listdir(input_folder):
 
 merger.write(f'output/{output_file_name}')
 merger.close()
+
+# ask if user wants to delete the files in the /input folder
+delete_files = input('Permanently delete files in input folder? (y/n): ')
+if delete_files.lower() == 'y':
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.pdf'):
+            os.remove(f'{input_folder}/' + filename)
